a07fee7565958dc9486522a6862758d424817f4e
Juan Sebastian Guerra Alvarez <juansebastian2014.jsga@gmail.com>
2023-05-11 00:18:06 -0500

    Animacion home lista

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,11 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-//Fondo
-public imagePath = 'assets/img/Fondo.jpg';
+export class HomeComponent implements OnInit {
+
+  animatedText: string = '';
+  currentIndex: number = 0;
+  currentPhrase: string = '';
+  typingSpeed: number = 150; // Velocidad de escritura en milisegundos
+  deletingSpeed: number = 100; // Velocidad de borrado en milisegundos
+  pauseBetweenPhrases: number = 500; // Pausa entre frases en milisegundos
+
+  phrases: string[] = [
+    '"Desarrollador web"',
+    '"Full Stack"'
+  ];
+
+  constructor() { }
+
+ngOnInit() {
+  this.startAnimation();
+}
+
+startAnimation() {
+  this.typePhrase();
+}
+
+typePhrase() {
+  if (this.currentIndex >= this.phrases.length) {
+    this.currentIndex = 0;
+  }
+
+  const currentPhrase = this.phrases[this.currentIndex];
+
+  let currentIndex = 0;
+  this.animatedText = '';
+  this.currentPhrase = currentPhrase;
+
+  const typingInterval = setInterval(() => {
+    this.animatedText = currentPhrase.substring(0, currentIndex + 1);
+    currentIndex++;
+
+    if (currentIndex >= currentPhrase.length) {
+      clearInterval(typingInterval);
+      setTimeout(() => {
+        this.deletePhrase();
+      }, this.pauseBetweenPhrases);
+    }
+  }, this.typingSpeed);
+}
+
+deletePhrase() {
+  const currentPhrase = this.currentPhrase;
+
+  let currentIndex = currentPhrase.length;
+  const deletingInterval = setInterval(() => {
+    this.animatedText = currentPhrase.substring(0, currentIndex);
+    currentIndex--;
+
+    if (currentIndex < 0) {
+      clearInterval(deletingInterval);
+      this.currentIndex++;
+      setTimeout(() => {
+        this.typePhrase();
+      }, this.pauseBetweenPhrases);
+    }
+  }, this.deletingSpeed);
+}
 }
